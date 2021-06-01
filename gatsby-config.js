3b6fe90e660ddfce51e51f96c3dd4870b1295759
Andrew Vargas <79120637+avarg35@users.noreply.github.com>
2021-06-01 10:24:52 -0400

    new favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitle: `AV `,
+    siteTitle: `AV`,
     siteTitleAlt: `Andrew Vargas`
   },
   plugins: [
