50ba69e56ab2e2892544c2270c199d90e9fc5ba1
Andrew Vargas <79120637+avarg35@users.noreply.github.com>
2021-06-01 00:34:15 -0400

    tweaked description a lil bit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitle: `Andrew Vargas `,
+    siteTitle: `AV `,
     siteTitleAlt: `Andrew Vargas`
   },
   plugins: [
